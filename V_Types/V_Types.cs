ccee2374dfcbb29aa0a9ca974693e24475ebc065
Mezer_Ilya <mr.mezer@myttk.ru>
2022-05-06 09:56:10 +0700

    Task1

---
+++ V_Types/V_Types.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V_Types
+{
+    internal class V_Types
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите максмальную погоду:");
+            decimal maxtemp = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Введите минимальную погоду:");
+            decimal mintemp = Convert.ToDecimal(Console.ReadLine());
+
+            decimal midtemp = (maxtemp + mintemp) / 2;
+
+
+            Console.WriteLine($"Средняя температура: {midtemp} ");
+
+            Console.WriteLine("Press ENTER to finish programm");
+            Console.ReadLine();
+        }
+    }
+}
