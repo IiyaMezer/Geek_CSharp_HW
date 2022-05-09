684df6a664645f946f3c799a0094de444bf2ba5a
Mezer_Ilya <mr.mezer@myttk.ru>
2022-05-09 08:55:05 +0700

    Task2

--- V_Types/V_Types.cs
+++ V_Types/V_Types.cs
@@ -8,7 +8,7 @@ namespace V_Types
 {
     internal class V_Types
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
             Console.Write("Введите максмальную погоду:");
             decimal maxtemp = Convert.ToDecimal(Console.ReadLine());
@@ -21,8 +21,53 @@ namespace V_Types
 
             Console.WriteLine($"Средняя температура: {midtemp} ");
 
-            Console.WriteLine("Press ENTER to finish programm");
-            Console.ReadLine();
-        }
+            Console.Write("Введите номер месяца:");
+            int month = Convert.ToInt32(Console.ReadLine());
+            string M_title = "";
+            switch (month)
+            {
+                case 1: M_title = "Jan";
+                    break;
+
+                case 2: M_title = "Feb";
+                    break;
+
+                case 3: M_title = "Mar";
+                    break;
+
+                case 4: M_title = "Apr";
+                    break;
+
+                case 5: M_title = "May";
+                    break;
+
+                case 6: M_title = "Jun";
+                    break;
+
+                case 7: M_title = "Jul";
+                    break;
+
+                case 8: M_title = "Aug";
+                    break;
+
+                case 9: M_title = "Sep";
+                    break;
+
+                case 10: M_title = "Oct";
+                    break;
+
+                case 11: M_title = "Nov";
+                    break;
+
+                case 12: M_title = "Dec";
+                    break;       
+            }
+
+           Console.WriteLine(M_title);
+
+           Console.WriteLine("Press ENTER to finish programm");
+           Console.ReadLine();
+            
+        }                  
     }
 }
