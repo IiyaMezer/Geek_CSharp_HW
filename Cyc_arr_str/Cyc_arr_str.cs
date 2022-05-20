0992fcb6ad8d21cd83a4c07339251cf7dbc65379
Mezer_Ilya <mr.mezer@myttk.ru>
2022-05-20 17:29:36 +0700

    added menu

--- Cyc_arr_str/Cyc_arr_str.cs
+++ Cyc_arr_str/Cyc_arr_str.cs
@@ -31,7 +31,7 @@ namespace Cyc_arr_str
                 }
                 Console.WriteLine();
             }
-                Console.ReadKey();
+            Console.ReadKey();
         }
 
         static void Task01()
@@ -45,8 +45,8 @@ namespace Cyc_arr_str
             int X1 = Matrix.GetLength(0);
             int Y1 = Matrix.GetLength(1);
 
-            decimal ERR = 0;    
-            decimal dErr = (Y1+1)/(X1+1);
+            decimal ERR = 0;
+            decimal dErr = (Y1 + 1) / (X1 + 1);
             int y = Y0;
             int diry = Y1;
 
@@ -74,59 +74,69 @@ namespace Cyc_arr_str
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Выберите задачу:");
-            Console.WriteLine("1 - Задача 1. Для квадранного массива");
-            Console.WriteLine("2 - Задача 1 С применением алгоритма Брезенхема(гугл помог_) in process.");
-            Console.WriteLine("3 - Задача 3.");
-            Console.WriteLine("4 - Задача 4.");
-            Console.WriteLine("===================");
-            int TaskNum = Convert.ToInt32(Console.ReadLine());
-            switch (TaskNum)
+            bool f = true;
+            while (f)
             {
-                case 1:
-                    Console.WriteLine("========================");
-                    Console.WriteLine("Задача 1");
-                    Console.WriteLine("========================");
-                    Task011();
-                    Console.WriteLine("========================");
-                    break;
+                Console.WriteLine("Выберите задачу:");
+                Console.WriteLine("1 - Задача 1. Для квадранного массива");
+                Console.WriteLine("2 - Задача 1 С применением алгоритма Брезенхема(гугл помог_) in process.");
+                Console.WriteLine("3 - Задача 3.");
+                Console.WriteLine("4 - Задача 4.");
+                Console.WriteLine("===================");
+                int TaskNum = Convert.ToInt32(Console.ReadLine());
+                switch (TaskNum)
+                {
+                    case 1:
+                        Console.WriteLine("========================");
+                        Console.WriteLine("Задача 1");
+                        Console.WriteLine("========================");
+                        Task011();
+                        Console.WriteLine("========================");
+                        break;
 
-                case 2:
-                    Console.WriteLine("========================");
-                    Console.WriteLine("Задача 1");
-                    Console.WriteLine("========================");
-                    Task01();
-                    Console.WriteLine("========================");
-                    break;
+                    case 2:
+                        Console.WriteLine("========================");
+                        Console.WriteLine("Задача 1");
+                        Console.WriteLine("========================");
+                        Task01();
+                        Console.WriteLine("========================");
+                        break;
 
-                case 0:
-                    Console.WriteLine("========================");
-                    Console.WriteLine("Звершение работы");
-                    Console.WriteLine("========================");
-                    Console.ReadKey();
-                    return;
-
-                case 3:
-                    Console.WriteLine("========================");
-                    Console.WriteLine("Задача 3");
-                    Console.WriteLine("========================");
-                    Task03();
-                    Console.WriteLine("========================");
-                    break;
-
-                case 4:
-                    Console.WriteLine("========================");
-                    Console.WriteLine("Задача 4");
-                    Console.WriteLine("========================");
-                    Task04();
-                    Console.WriteLine("========================");
-                    break;
+                    case 0:
+                        f = false;
+                        Console.WriteLine("========================");
+                        Console.WriteLine("Звершение работы");
+                        Console.WriteLine("========================");
+                        Console.ReadKey();
+                        break;
+                        
 
 
+                        //case 3:
+                        //    Console.WriteLine("========================");
+                        //    Console.WriteLine("Задача 3");
+                        //    Console.WriteLine("========================");
+                        //    Task03();
+                        //    Console.WriteLine("========================");
+                        //    break;
+
+                        //case 4:
+                        //    Console.WriteLine("========================");
+                        //    Console.WriteLine("Задача 4");
+                        //    Console.WriteLine("========================");
+                        //    Task04();
+                        //    Console.WriteLine("========================");
+                        //    break;
+
+                }
 
 
 
-                    Console.ReadKey();
+
+
+                
+            }
         }
+
     }
 }
