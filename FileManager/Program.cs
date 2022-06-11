64afe55992447a00a22b6d6b694165618c4e7062
Mezer_Ilya <mr.mezer@myttk.ru>
2022-06-11 18:17:38 +0700

    1

--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -83,6 +83,7 @@ namespace FileManager
                     }
                     else
                     {
+                        command.Clear();
                         Console.SetCursorPosition(left, top);
                     }
 
